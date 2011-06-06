4e330a3cd938d7320a8a3d42356f10bef46f6cb5
Thomas Massmann <thomas@propertyshelf.com>
2011-06-06 14:45:11 +0200

    Fixed jQuery selector. Only grok the dexterity content package.

--- src/plone/mls/listing/browser/javascript/listing_view.js
+++ src/plone/mls/listing/browser/javascript/listing_view.js
@@ -1,16 +1,16 @@
-$(document).ready(function() {
+jq(document).ready(function() {
 
   // Build JS Gallery
-  if ($('#listing-images .thumbnails').length > 0) {
+  if (jq('#listing-images .thumbnails').length > 0) {
     // Load the theme ones more. This is necessary for mobile devices.
     Galleria.loadTheme('++resource++plone.mls.listing.javascript/classic/galleria.classic.min.js');
-    $('#listing-images a.preview').replaceWith('<div id="galleria"></div>');
+    jq('#listing-images a.preview').replaceWith('<div id="galleria"></div>');
 
     // Hide the thumbnails
-    $('#listing-images .thumbnails').hide();
+    jq('#listing-images .thumbnails').hide();
 
     // Initialize Galleria.
-    var galleria_obj = $('#galleria').galleria({
+    var galleria_obj = jq('#galleria').galleria({
       dataSource: '.thumbnails',
       width: 410,
       height: 400,
