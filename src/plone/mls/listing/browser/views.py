15a3419d3cef20ba46e26c85625379c6f2e82e76
Thomas Massmann <thomas@propertyshelf.com>
2011-12-24 14:05:48 +0100

    Added recent listings view skeleton.

---
+++ src/plone/mls/listing/browser/views.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+###############################################################################
+#
+# Copyright (c) 2011 Propertyshelf, Inc. and its Contributors.
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License version 2 as published by the
+# Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+###############################################################################
+"""Various stand alone browser views for listings."""
+
+# zope imports
+from zope.publisher.browser import BrowserView
+
+
+class RecentListings(BrowserView):
+    """Shows recent (active) listings from the MLS."""
