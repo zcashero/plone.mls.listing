f6947f09f81d258955b1f58965b037c79920c07c
Thomas Massmann <thomas@propertyshelf.com>
2012-07-31 09:35:51 +0200

    Added quick search form to quick search portlet.

--- src/plone/mls/listing/portlets/quick_search.py
+++ src/plone/mls/listing/portlets/quick_search.py
@@ -20,19 +20,58 @@
 """Agent Information Portlet."""
 
 # zope imports
+from Acquisition import aq_inner
 from plone.app.portlets.portlets import base
+from plone.directives import form
 from plone.portlets.interfaces import IPortletDataProvider
-from zope import schema
-from zope.formlib import form
-from zope.interface import implementer
+from plone.z3cform import z2
+from z3c.form import field
+from z3c.form.browser import checkbox, radio
+from z3c.form.interfaces import IFormLayer
+from zope import formlib, schema
+from zope.interface import alsoProvides, implementer
 from zope.schema.fieldproperty import FieldProperty
 
 # local imports
-from plone.mls.listing.browser.interfaces import IListingDetails
+from plone.mls.listing.browser.listing_search import IListingSearchForm
+from plone.mls.listing.browser.valuerange.widget import ValueRangeFieldWidget
 from plone.mls.listing.i18n import _
+
+# starting from 0.6.0 version plone.z3cform has IWrappedForm interface
+try:
+    from plone.z3cform.interfaces import IWrappedForm
+    HAS_WRAPPED_FORM = True
+except ImportError:
+    HAS_WRAPPED_FORM = False
 
 
 MSG_PORTLET_DESCRIPTION = _(u'This portlet shows a listing quick search form.')
+
+
+class QuickSearchForm(form.Form):
+    """Quick Search Form."""
+    fields = field.Fields(IListingSearchForm)
+    ignoreContext = True
+
+    fields['listing_type'].widgetFactory = checkbox.CheckBoxFieldWidget
+    fields['location_type'].widgetFactory = checkbox.CheckBoxFieldWidget
+    fields['object_type'].widgetFactory = checkbox.CheckBoxFieldWidget
+    fields['baths'].widgetFactory = ValueRangeFieldWidget
+    fields['beds'].widgetFactory = ValueRangeFieldWidget
+    #additional fields for filtering
+    fields['geographic_type'].widgetFactory = checkbox.CheckBoxFieldWidget
+    fields['view_type'].widgetFactory = checkbox.CheckBoxFieldWidget
+    fields['ownership_type'].widgetFactory = checkbox.CheckBoxFieldWidget
+
+    fields['air_condition'].widgetFactory = radio.RadioFieldWidget
+    fields['jacuzzi'].widgetFactory = radio.RadioFieldWidget
+    fields['pool'].widgetFactory = radio.RadioFieldWidget
+
+    def __init__(self, context, request, portlet_hash=None, data=None):
+        super(QuickSearchForm, self).__init__(context, request)
+        self.data = data
+        if portlet_hash:
+            self.prefix = portlet_hash + '.' + self.prefix
 
 
 class IQuickSearchPortlet(IPortletDataProvider):
@@ -61,32 +100,36 @@ class Assignment(base.Assignment):
 class Renderer(base.Renderer):
     """Agent Information Portlet Renderer."""
 
-    # @property
-    # def available(self):
-    #     return IListingDetails.providedBy(self.view) and \
-    #            getattr(self.view, 'listing_id', None) is not None
-
     @property
     def title(self):
         if self.data.heading is not None:
             return self.data.heading
         return self.data.title
 
+    def update(self):
+        z2.switch_on(self, request_layer=IFormLayer)
+        portlet_hash = self.__portlet_metadata__.get('hash')
+        self.form = QuickSearchForm(aq_inner(self.context), self.request,
+                              portlet_hash)
+        if HAS_WRAPPED_FORM:
+            alsoProvides(self.form, IWrappedForm)
+        self.form.update()
+
 
 class AddForm(base.AddForm):
     """Add form for the Agent Information portlet."""
-    form_fields = form.Fields(IQuickSearchPortlet)
+    form_fields = formlib.form.Fields(IQuickSearchPortlet)
     label = _(u'Add Agent Information portlet')
     description = MSG_PORTLET_DESCRIPTION
 
     def create(self, data):
         assignment = Assignment()
-        form.applyChanges(assignment, self.form_fields, data)
+        formlib.form.applyChanges(assignment, self.form_fields, data)
         return assignment
 
 
 class EditForm(base.EditForm):
     """Edit form for the Agent Information portlet."""
-    form_fields = form.Fields(IQuickSearchPortlet)
+    form_fields = formlib.form.Fields(IQuickSearchPortlet)
     label = _(u'Edit Agent Information portlet')
     description = MSG_PORTLET_DESCRIPTION
