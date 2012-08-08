6f63e2bafcc8a6c42fc6d527d413cd16bc7a9890
Thomas Massmann <thomas@propertyshelf.com>
2012-08-08 17:47:32 +0200

    Show custom agent info if 3rd party listing and option for showing custom info is selected.

--- src/plone/mls/listing/browser/views.py
+++ src/plone/mls/listing/browser/views.py
@@ -20,14 +20,17 @@
 """Various browser views for listings."""
 
 # zope imports
-from plone.memoize.view import memoize
-from zope.component import queryMultiAdapter
-from zope.interface import implements
 from Products.Five import BrowserView
+from plone.memoize.view import memoize
+from plone.registry.interfaces import IRegistry
+from zope.component import getUtility, queryMultiAdapter
+from zope.interface import implements
 
 # local imports
+from plone.mls.core.interfaces import IMLSSettings
 from plone.mls.listing.api import listing_details
 from plone.mls.listing.browser.interfaces import IListingDetails
+from plone.mls.listing.interfaces import IMLSAgencyContactInformation
 
 
 class ListingDetails(BrowserView):
@@ -117,5 +120,104 @@ class ListingDetails(BrowserView):
 
     @property
     def contact(self):
-        if self.data is not None:
-            return self.data.get('contact', None)
+        registry = getUtility(IRegistry)
+        mls_settings = registry.forInterface(IMLSSettings)
+        agency_id = getattr(mls_settings, 'agency_id', None)
+
+        if self.data is None:
+            return
+
+        contact_data = self.data.get('contact', None)
+        agency = contact_data.get('agency', {})
+        agent = contact_data.get('agent', {})
+
+        if agency.get('id', {}).get('value', None) == agency_id:
+            return contact_data
+
+        settings = registry.forInterface(IMLSAgencyContactInformation)
+        if getattr(settings, 'use_custom_info', False) is True:
+
+            # Adjust agency name.
+            agency_name = getattr(settings, 'agency_name', None)
+            if agency_name is not None:
+                item = agency.setdefault('name', {})
+                item['value'] = agency_name
+            else:
+                agency['name'] = None
+
+            # Adjust agency logo.
+            agency_logo = getattr(settings, 'agency_logo_url', None)
+            if agency_logo is not None:
+                agency['logo'] = agency_logo
+            else:
+                agency['logo'] = None
+
+            # Adjust agency office phone.
+            agency_office_phone = \
+                getattr(settings, 'agency_office_phone', None)
+            if agency_office_phone is not None:
+                item = agency.setdefault('office_phone', {})
+                item['value'] = agency_office_phone
+            else:
+                agency['office_phone'] = None
+
+            # Adjust agency website.
+            agency_website = getattr(settings, 'agency_website', None)
+            if agency_website is not None:
+                item = agency.setdefault('website', {})
+                item['value'] = agency_website
+            else:
+                agency['website'] = None
+
+            # Adjust agent name.
+            agent_name = getattr(settings, 'agent_name', None)
+            if agent_name is not None:
+                item = agent.setdefault('name', {})
+                item['value'] = agent_name
+            else:
+                agent['name'] = None
+
+            # Adjust agent title.
+            agent_title = getattr(settings, 'agent_title', None)
+            if agent_title is not None:
+                item = agent.setdefault('title', {})
+                item['value'] = agent_title
+            else:
+                agent['title'] = None
+
+            # Adjust agent office phone.
+            agent_office_phone = getattr(settings, 'agent_office_phone', None)
+            if agent_office_phone is not None:
+                item = agent.setdefault('agent_office_phone', {})
+                item['value'] = agent_office_phone
+            else:
+                agent['agent_office_phone'] = None
+
+            # Adjust agent cell phone.
+            agent_cell_phone = getattr(settings, 'agent_cell_phone', None)
+            if agent_cell_phone is not None:
+                item = agent.setdefault('agent_cell_phone', {})
+                item['value'] = agent_cell_phone
+            else:
+                agent['agent_cell_phone'] = None
+
+            # Adjust agent fax.
+            agent_fax = getattr(settings, 'agent_fax', None)
+            if agent_fax is not None:
+                item = agent.setdefault('agent_fax', {})
+                item['value'] = agent_fax
+            else:
+                agent['agent_fax'] = None
+
+            # Adjust agent email.
+            agent_email = getattr(settings, 'agent_email', None)
+            if agent_email is not None:
+                item = agent.setdefault('agent_email', {})
+                item['value'] = agent_email
+            else:
+                agent['agent_email'] = None
+
+            # TODO: Adjust agent languages.
+            # TODO: Adjust agent avatar.
+
+        return contact_data
