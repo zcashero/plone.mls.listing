cf251819cc438f9ec3e5e6b2d273f110d6b51b44
Thomas Massmann <thomas@propertyshelf.com>
2013-06-10 22:57:02 +0200

    [MLSPE-15] Hide contact info for agent info portlet if contact portlet is available.

--- src/plone/mls/listing/portlets/agent_information.py
+++ src/plone/mls/listing/portlets/agent_information.py
@@ -21,8 +21,13 @@
 
 # zope imports
 from plone.app.portlets.portlets import base
-from plone.portlets.interfaces import IPortletDataProvider
+from plone.portlets.interfaces import (
+    IPortletDataProvider,
+    IPortletManager,
+    IPortletRetriever,
+)
 from zope import schema
+from zope.component import getMultiAdapter, getUtility
 from zope.formlib import form
 from zope.interface import implementer
 from zope.schema.fieldproperty import FieldProperty
@@ -30,6 +35,7 @@ from zope.schema.fieldproperty import FieldProperty
 # local imports
 from plone.mls.listing.browser.interfaces import IListingDetails
 from plone.mls.listing.i18n import _
+from plone.mls.listing.portlets.agent_contact import IAgentContactPortlet
 
 
 MSG_PORTLET_DESCRIPTION = _(
@@ -75,6 +81,17 @@ class Renderer(base.Renderer):
         if self.data.heading is not None:
             return self.data.heading
         return self.data.title
+
+    @property
+    def agent_contact_portlet_available(self):
+        for column in ["plone.leftcolumn", "plone.rightcolumn"]:
+            manager = getUtility(IPortletManager, name=column)
+            retriever = getMultiAdapter((self.context, manager), IPortletRetriever)
+            portlets = retriever.getPortlets()
+            for portlet in portlets:
+                if IAgentContactPortlet.providedBy(portlet["assignment"]):
+                    return True
+        return False
 
 
 class AddForm(base.AddForm):
