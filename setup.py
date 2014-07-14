17c98581b0324f9c6beb6a78448070480c804ff7
Thomas Massmann <thomas@propertyshelf.com>
2014-07-14 14:37:26 +0200

    Back to development.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 from setuptools import setup, find_packages
 
-version = '0.9.12'
+version = '1.0dev'
 description = "Plone support for MLS Listings."
 long_description = ('\n'.join([
     open('README.rst').read(),
