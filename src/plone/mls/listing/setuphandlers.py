18a621a897ea5301c26d91f6b8a687f0a0434ef3
Thomas Massmann <thomas@propertyshelf.com>
2014-11-17 15:29:25 +0100

    Removed kupu integration.

--- src/plone/mls/listing/setuphandlers.py
+++ src/plone/mls/listing/setuphandlers.py
@@ -9,34 +9,6 @@ from zope.component import getUtility
 LISTING_TYPE = 'plone.mls.listing.listing'
 
 
-def setup_kupu(context):
-    """Set up Kupu."""
-    if not context.readDataFile('plone.mls.listing_various.txt'):
-        return
-
-    site = getUtility(IPloneSiteRoot)
-
-    portal_types = getToolByName(site, 'portal_types')
-    kupu = getToolByName(site, 'kupu_library_tool', None)
-    if kupu is not None:
-        linkable = list(kupu.getPortalTypesForResourceType('linkable'))
-        if LISTING_TYPE not in linkable:
-            # Kupu's resource list can accumulate old, no longer valid types.
-            # It will throw an exception if we try to resave them.
-            # So, let's clean the list.
-            valid_types = dict([
-                (t.id, 1) for t in portal_types.listTypeInfo()
-            ])
-            linkable = [pt for pt in linkable if pt in valid_types]
-
-            linkable.append(LISTING_TYPE)
-            kupu.updateResourceTypes(({
-                'resource_type': 'linkable',
-                'old_type': 'linkable',
-                'portal_types': linkable,
-            },))
-
-
 def setup_article(context):
     """Set up raptus.article."""
     if not context.readDataFile('plone.mls.listing_various.txt'):
