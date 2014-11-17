ad2bc6c168527a2354c60fb52ebcf55ab2ff5f1c
Thomas Massmann <thomas@propertyshelf.com>
2014-11-17 15:30:29 +0100

    Removed setup handler for versioning.
    This is already done using the repositorytool.xml.

--- src/plone/mls/listing/setuphandlers.py
+++ src/plone/mls/listing/setuphandlers.py
@@ -23,29 +23,3 @@ def setup_article(context):
             return
         if LISTING_TYPE not in article.allowed_content_types:
             article.allowed_content_types += (LISTING_TYPE, )
-
-
-def setup_versioning(context):
-    """Enable versioning in portal types."""
-    if not context.readDataFile('plone.mls.listing_various.txt'):
-        return
-
-    try:
-        from Products.CMFEditions.setuphandlers import DEFAULT_POLICIES
-        # we're on plone < 4.1, configure versionable types manually
-    except ImportError:
-        # repositorytool.xml will be used
-        pass
-    else:
-        site = getUtility(IPloneSiteRoot)
-        portal_repository = getToolByName(site, 'portal_repository')
-        versionable = list(portal_repository.getVersionableContentTypes())
-        if LISTING_TYPE not in versionable:
-            # Use append() to make sure we don't overwrite any content types
-            # which may already be under version control.
-            versionable.append(LISTING_TYPE)
-            # Add default versioning policies to the versioned type.
-            for policy_id in DEFAULT_POLICIES:
-                portal_repository.addPolicyForContentType(LISTING_TYPE,
-                                                          policy_id)
-        portal_repository.setVersionableContentTypes(versionable)
