893bb906a3ea7cd3acb3085ac548c75615a64a38
Thomas Massmann <thomas@propertyshelf.com>
2014-11-19 20:09:36 +0100

    Updated install tests.

--- src/plone/mls/listing/tests/test_setup.py
+++ src/plone/mls/listing/tests/test_setup.py
@@ -12,17 +12,23 @@ class TestSetup(unittest.TestCase):
     """Setup Test Case for plone.mls.listing."""
     layer = PLONE_MLS_LISTING_INTEGRATION_TESTING
 
-    def test_plone_mls_core_installed(self):
-        """Test that plone.mls.core is installed."""
+    def test_collective_prettyphoto_installed(self):
+        """Validate that collective.prettyphoto is installed."""
         portal = self.layer['portal']
         qi = portal.portal_quickinstaller
-        self.assertTrue(qi.isProductInstalled('plone.mls.core'))
+        self.assertTrue(qi.isProductInstalled('collective.prettyphoto'))
 
     def test_plone_app_dexterity_installed(self):
-        """Test that plone.app.dexterity is installed."""
+        """Validate that plone.app.dexterity is installed."""
         portal = self.layer['portal']
         qi = portal.portal_quickinstaller
         self.assertTrue(qi.isProductInstalled('plone.app.dexterity'))
+
+    def test_plone_mls_core_installed(self):
+        """Validate that plone.mls.core is installed."""
+        portal = self.layer['portal']
+        qi = portal.portal_quickinstaller
+        self.assertTrue(qi.isProductInstalled('plone.mls.core'))
 
     def test_ps_plone_fotorama_installed(self):
         """Validate that ps.plone.fotorama is installed."""
