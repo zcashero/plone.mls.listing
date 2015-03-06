8b4489614f4bb49c8e27c4183af95f7728df3730
Thomas Massmann <thomas.massmann@it-spir.it>
2015-03-06 17:25:50 +0100

    Added custom registry edit form which fixes corrupted schema registrations.

--- src/plone/mls/listing/browser/controlpanel.py
+++ src/plone/mls/listing/browser/controlpanel.py
@@ -3,16 +3,39 @@
 
 # zope imports
 from plone.app.registry.browser import controlpanel
+from plone.registry.interfaces import IRegistry
+from z3c.form import field
+from zope.component import getUtility
 
 # local imports
-# from plone.mls.listing.i18n import _
+from plone.mls.listing.i18n import _
 from plone.mls.listing.interfaces import (
     IMLSAgencyContactInformation,
     IMLSUISettings,
 )
 
 
-class MLSAgencyContactInfoSettingsEditForm(controlpanel.RegistryEditForm):
+class SelfHealingRegistryEditForm(controlpanel.RegistryEditForm):
+    """Registers the schema if an error occured."""
+
+    def getContent(self):
+        registry = getUtility(IRegistry)
+        try:
+            return registry.forInterface(
+                self.schema,
+                prefix=self.schema_prefix,
+            )
+        except KeyError:
+            self.ignoreContext = True
+            self.fields = field.Fields()
+            registry.registerInterface(self.schema)
+            self.status = _(
+                u'Registry has been updated. Please reload this page.'
+            )
+            return None
+
+
+class MLSAgencyContactInfoSettingsEditForm(SelfHealingRegistryEditForm):
     """MLS Agency Contact Info Settings Form."""
 
     schema = IMLSAgencyContactInformation
@@ -32,7 +55,7 @@ class MLSAgencyContactInfoSettingsControlPanel(
     form = MLSAgencyContactInfoSettingsEditForm
 
 
-class MLSUISettingsEditForm(controlpanel.RegistryEditForm):
+class MLSUISettingsEditForm(SelfHealingRegistryEditForm):
     """MLS UI Settings Form."""
 
     schema = IMLSUISettings
