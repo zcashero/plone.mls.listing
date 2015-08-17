5fd4eafa4d5ebec484760c4bd42d830c3b53dc93
Jens Krause <jens@propertyshelf.com>
2015-08-17 18:58:50 +0700

    try different map position

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 from setuptools import setup, find_packages
 
-version = '0.9.23dev2'
+version = '0.9.23dev3'
 description = 'Plone support for MLS Listings.'
 long_description = ('\n'.join([
     open('README.rst').read(),
