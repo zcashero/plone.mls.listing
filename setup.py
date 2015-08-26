7ba4962aee080366c7e82c6657582823a83439b6
Jens Krause <jens@propertyshelf.com>
2015-08-26 15:05:33 +0700

    count up dev-nr

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 from setuptools import setup, find_packages
 
-version = '0.9.23dev4'
+version = '0.9.23dev5'
 description = 'Plone support for MLS Listings.'
 long_description = ('\n'.join([
     open('README.rst').read(),
