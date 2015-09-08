95c5e3c0678d9b30fc85749e2a500fbf7d9267ab
Jens Krause <jens@propertyshelf.com>
2015-09-08 22:49:38 +0700

    revert location multi-select checkboxes for Quicksearch portlet

--- src/plone/mls/listing/portlets/quick_search.py
+++ src/plone/mls/listing/portlets/quick_search.py
@@ -78,9 +78,6 @@ class QuickSearchForm(form.Form):
     method = 'get'
 
     fields['listing_type'].widgetFactory = checkbox.CheckBoxFieldWidget
-    fields['location_state'].widgetFactory = checkbox.CheckBoxFieldWidget
-    fields['location_district'].widgetFactory = checkbox.CheckBoxFieldWidget
-    fields['location_county'].widgetFactory = checkbox.CheckBoxFieldWidget
     fields['location_type'].widgetFactory = checkbox.CheckBoxFieldWidget
     fields['object_type'].widgetFactory = checkbox.CheckBoxFieldWidget
     fields['baths'].widgetFactory = ValueRangeFieldWidget
